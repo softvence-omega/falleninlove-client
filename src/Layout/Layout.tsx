9f3571323cbb440eb67fa1e7288faa7eb6a25ea0
ashimsarkar1512 <ashimsarkar5558@gmail.com>
2025-08-23 13:46:23 +0600

    updated  emergencyaccess

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -1,16 +1,16 @@
 
 import { Outlet } from "react-router-dom";
 
-import UserNavbar from "@/components/User/UserNavbar";
+
 
 const Layout: React.FC = () => {
   return (
     <div>
-      <UserNavbar></UserNavbar>
+
       <main>
         <Outlet />
       </main>
-     
+
     </div>
   );
 };
--- src/routes/UserRoutes.tsx
+++ src/routes/UserRoutes.tsx
@@ -2,24 +2,25 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Sidebar from "@/components/reusable/SideBar";
+import UserNavbar from "@/components/User/UserNavbar";
 
 const UserRoute: React.FC = () => {
   return (
     <div className="flex min-h-screen mt-16">
-      
 
-    
-     
- <aside>
-    <Sidebar role="user" brandName="CareBot" />
- </aside>
+      <UserNavbar></UserNavbar>
+
+
+      <aside>
+        <Sidebar role="user" brandName="CareBot" />
+      </aside>
 
       {/* Main Content */}
       <main className="flex-1  p-6">
         <Outlet />
       </main>
     </div>
-   
+
   );
 };
 
