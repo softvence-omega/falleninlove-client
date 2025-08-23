83294cbdc8dc005f10433ffbf4b0124225ef4b15
akashmuhammadabrrar <madborakash48@gmail.com>
2025-08-23 09:48:07 +0600

    this commit for the fixing all routes

---
+++ src/components/Documents.tsx
@@ -0,0 +1,6 @@
+
+export default function Documents() {
+  return (
+    <div>Documents</div>
+  )
+}
---
+++ src/routes/UserRoutes.tsx
@@ -0,0 +1,22 @@
+// src/routes/UserRoute.tsx
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Sidebar from "@/components/reusable/SideBar";
+
+const UserRoute: React.FC = () => {
+  return (
+    <div className="flex min-h-screen mt-16">
+      {/* Sidebar */}
+      <aside className="hidden lg:block w-64 bg-white shadow-lg">
+        <Sidebar role="user" brandName="CareBot" className="h-full" />
+      </aside>
+
+      {/* Main Content */}
+      <main className="flex-1 bg-gray-50 p-6">
+        <Outlet />
+      </main>
+    </div>
+  );
+};
+
+export default UserRoute;
