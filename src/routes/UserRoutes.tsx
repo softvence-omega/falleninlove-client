b9673fc069156bdbd771b2883ebbc118d91eec1c
akashmuhammadabrrar <madborakash48@gmail.com>
2025-08-23 11:03:24 +0600

    this commit for the sidebar and routes

--- src/routes/UserRoutes.tsx
+++ src/routes/UserRoutes.tsx
@@ -6,10 +6,7 @@ import Sidebar from "@/components/reusable/SideBar";
 const UserRoute: React.FC = () => {
   return (
     <div className="flex min-h-screen mt-16">
-      {/* Sidebar */}
-      <aside className="hidden lg:block w-64 bg-white shadow-lg">
-        <Sidebar role="user" brandName="CareBot" className="h-full" />
-      </aside>
+      <Sidebar role="user" brandName="CareBot" />
 
       {/* Main Content */}
       <main className="flex-1 bg-gray-50 p-6">
